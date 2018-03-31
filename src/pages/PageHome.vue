2c66bcdef3ee4e3ff815c85510fb9f200c34f64d
Camilo Segura <camilosegurar@gmail.com>
2018-03-31 09:45:48 -0500

    feat:  Added list of threads and show each thread

---
+++ src/pages/PageHome.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="col-full">
+    <h1>Welcome to Camilo's forum</h1>
+    <ThreadList :threads="threads" />
+  </div>
+</template>
+
+<script>
+  import sourceData from '@/data'
+  import ThreadList from '@/components/ThreadList'
+
+  export default {
+    components: {
+      ThreadList
+    },
+    data () {
+      return {
+        threads: Object.values(sourceData.threads),
+        posts: sourceData.posts,
+        users: sourceData.users
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ src/pages/PageThreadShow.vue
@@ -0,0 +1,45 @@
+<template>
+  <article class="col-large push-top">
+    <h1>{{thread.title}}</h1>
+    <main class="post-list">
+      <div v-for="postId in thread.posts" :key="postId" class="post">
+        <div class="user-info">
+          <a href="#" class="user-name">{{users[posts[postId].userId].name}}</a>
+          <a href="#">
+            <img :src="users[posts[postId].userId].avatar" alt="" class="avatar-large">
+          </a>
+        </div>
+
+        <p class="post-content">
+          {{posts[postId].text}}
+        </p>
+
+        <time class="post-date text-faded">
+          {{posts[postId].publishedAt}}
+        </time>
+      </div>
+    </main>
+
+  </article>
+</template>
+
+<script>
+  import sourceData from '@/data'
+
+  export default {
+    props: {
+      id: {
+        required: true,
+        type: String
+      }
+    },
+    data () {
+      return {
+        thread: sourceData.threads[this.id],
+        posts: sourceData.posts,
+        users: sourceData.users
+      }
+    }
+  }
+</script>
+
